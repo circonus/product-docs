9c6037d440c1f063d822031769fea8607a4f28c0
Will Krause <will.krause@circonus.com>
2023-08-07 14:07:12 -0400

    update build to ignore broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -21,7 +21,7 @@ const config = {
   organizationName: "Circonus", // Usually your GitHub org/user name.
   projectName: "circonus-docs", // Usually your repo name.
 
-  onBrokenLinks: "throw",
+  onBrokenLinks: "warn",
   onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internalization, you can use this field to set useful
